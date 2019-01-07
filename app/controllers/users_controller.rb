a4b17d2175bad255981d47f5371cf51866b58ac8
youhei.koni <1104crf14@gmail.com>
2019-01-07 06:39:15 +0000

    user_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if logged_in?
-      @events = current_user.events.where("event_start > ?", Time.now).order('created_at DESC') 
+      @event = @user.events.where("event_start > ?", Time.now).order('created_at DESC')
     end
   end
 
